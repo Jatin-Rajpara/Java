f1ed85ee5a8b5419549842913c8da0caee80b06e
Rajpara Jatin <jatinrajpara2@gmail.com>
2026-07-14 12:21:16 +0530

    Update solutation4.java

--- session16/solutation4.java
+++ session16/solutation4.java
@@ -1,14 +1,14 @@
 package session16;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 
 public class solutation4 {
 
     public static void main(String[] args) {
 
         HashMap<String, Integer> followers = new HashMap<String, Integer>();
-
+      
         followers.put("virat", 270000000);
         followers.put("cristiano", 650000000);
         followers.put("selenagomez", 420000000);
@@ -16,23 +16,25 @@ public class solutation4 {
         followers.put("leomessi", 510000000);
 
         int maxFollowers = 0;
+        ArrayList<String> usernames = new ArrayList<String>(followers.keySet());
 
-        for (int count : followers.values()) {
-
+        for (int i = 0; i < usernames.size(); i++) {
+            String user = usernames.get(i);
+            int count = followers.get(user); 
+            
             if (count > maxFollowers) {
                 maxFollowers = count;
             }
-
         }
 
         System.out.println("User(s) with Highest Followers:");
 
-        for (Map.Entry<String, Integer> entry : followers.entrySet()) {
+        for (int i = 0; i < usernames.size(); i++) {
+            String user = usernames.get(i);
+            int count = followers.get(user);
 
-            if (entry.getValue() == maxFollowers) {
-
-                System.out.println(entry.getKey() + " : " + entry.getValue());
-
+            if (count == maxFollowers) {
+                System.out.println(user + " : " + count);
             }
         }
     }
