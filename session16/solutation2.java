e237408ac2aea4a0f28f12399dad9477b58f001e
Rajpara Jatin <jatinrajpara2@gmail.com>
2026-07-14 12:13:48 +0530

    Update solutation2.java
    Replaced usernames with emails and added iterator for unique email listing.

--- session16/solutation2.java
+++ session16/solutation2.java
@@ -1,27 +1,27 @@
 package session16;
 
 import java.util.HashSet;
+import java.util.Iterator; // Yeh naya import add kiya hai
 
 public class solutation2 {
 
     public static void main(String[] args) {
 
-        HashSet<String> users = new HashSet<String>();
+        HashSet<String> emails = new HashSet<String>();
 
-        users.add("man123");
-        users.add("rahul99");
-        users.add("priya07");
-        users.add("amit21");
-        users.add("neha88");
-        users.add("rahul99");
-        users.add("man123");
+        emails.add("amit@gmail.com");
+        emails.add("pooja@gmail.com");
+        emails.add("rohit@yahoo.com");
+        emails.add("amit@gmail.com"); 
+        emails.add("sneha@gmail.com");
+        emails.add("rohit@yahoo.com"); 
 
-        System.out.println("Unique Usernames:");
-
-        for (String user : users) {
-
-            System.out.println(user);
-
+        System.out.println("Unique Email List:");
+        Iterator<String> it = emails.iterator();
+        
+        while (it.hasNext()) {
+            String email = it.next();
+            System.out.println(email);
         }
     }
 }
