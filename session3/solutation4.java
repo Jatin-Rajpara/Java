48def812483756917da0a346f523e2d9493e0f74
Rajpara Jatin <jatinrajpara2@gmail.com>
2026-07-14 11:49:53 +0530

    Update solutation4.java

--- session3/solutation4.java
+++ session3/solutation4.java
@@ -3,23 +3,15 @@ package session3;
 public class solutation4 {
     public static void main(String[] args) {
         
-    //         int cartTotal = 450;
-
-    // if (cartTotal >= 500) {
-    //     System.out.println("Eligible for Free Delivery");
-    // } else {
-    // System.out.println("Add more items for Free Delivery");}
-
-
         int cartTotal = 450;
         if (cartTotal > 0) {
             if (cartTotal >= 500) {
-                System.out.println("Eligible for Free Delivery");
+                System.out.println("Free Delivery...");
             } else {
-                System.out.println("Add more items for Free Delivery");
+                System.out.println("Add more items for Free Delivery...");
             }
         } else {
-            System.out.println("Cart is empty. Please add items to your cart.");
+            System.out.println("Cart is empty...");
         }
 
     }
