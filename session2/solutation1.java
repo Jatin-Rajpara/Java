2847c83415392ebbc59b39d58e93f45fa86c9a11
Rajpara Jatin <jatinrajpara2@gmail.com>
2026-07-14 11:52:46 +0530

    Update solutation1.java

--- session2/solutation1.java
+++ session2/solutation1.java
@@ -2,10 +2,10 @@ package session2;
 
 public class solutation1 {
     public static void main(String[] args) {
-        String productName = "Samsung Galaxy S25";
-        double price = 74999.99;
+        String productName = "Samsung Galaxy S26";
+        double price = 80000.99;
         boolean inStock = true;
-        float rating = 4.7f;
+        float rating = 4.9f;
 
         System.out.println("Product Name : " + productName);
         System.out.println("Price : ₹" + price);
