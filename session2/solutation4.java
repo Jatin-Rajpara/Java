8d8199ec626e69bc8b09958dbd7327b161c8ff85
Rajpara Jatin <jatinrajpara2@gmail.com>
2026-07-14 11:56:57 +0530

    Update solutation4.java

--- session2/solutation4.java
+++ session2/solutation4.java
@@ -1,18 +1,24 @@
 package session2;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class solutation4 {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter unread messages: ");
-    int messages = sc.nextInt();
-    String result = (messages == 0)
-    ? "No new messages"
-    : (messages <= 10)
-    ? "Few messages"
-    : "Too many messages";
-    System.out.println(result);
-}
-}
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter unread messages: ");
+        int msg = sc.nextInt();
+        
+        String result = "";
 
+       
+        if (msg == 0) {
+            result = "No new messages";
+        } else if (msg <= 10) {
+            result = "Few messages";
+        } else {
+            result = "Too many messages";
+        }
+
+        System.out.println(result);
+    }
+}
