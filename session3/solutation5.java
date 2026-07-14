bb84ef64e48749f938d2cd2086a8abd290d0fbd4
Rajpara Jatin <jatinrajpara2@gmail.com>
2026-07-14 11:51:49 +0530

    Update solutation5.java

--- session3/solutation5.java
+++ session3/solutation5.java
@@ -2,8 +2,8 @@ package session3;
 
 public class solutation5 {
 
-    public static void filterByMinRating(int[] ratings, int minRating) {
-        System.out.println("Ratings Greater Than or Equal to" + minRating + ":");
+    public static void min(int[] ratings, int minRating) {
+        System.out.println("Ratings : " + minRating + ":");
         for (int i = 0; i < ratings.length; i++) {
             if (ratings[i] >= minRating) {
                 System.out.println(ratings[i]);
@@ -12,8 +12,8 @@ public class solutation5 {
     }
     public static void main(String[] args) {
         
-        int[] ratings = { 4, 5, 3, 2, 5, 1, 4 };
-        int minRating = 4;
-        filterByMinRating(ratings, minRating);
+        int[] ratings = { 3,5,1,4,2,5,4 };
+        int minRating = 3;
+        min(ratings, minRating);
     }
 }
