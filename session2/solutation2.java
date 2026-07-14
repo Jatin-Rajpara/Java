3726c0787532a97076e7f145912599867a414339
Rajpara Jatin <jatinrajpara2@gmail.com>
2026-07-14 11:53:49 +0530

    Update solutation2.java
    Updated variable name from 'finalBalance' to 'final' for simplicity.

--- session2/solutation2.java
+++ session2/solutation2.java
@@ -1,20 +1,20 @@
 package session2;
-import java.util.Scanner;
+import java.util.*;
 public class solutation2 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     
-    System.out.print("Enter wallet balance: ₹");
+    System.out.print("Enter wallet balance: ");
         
         double balance = sc.nextDouble();
         double cashback = 0;
         if (balance > 500) {
         cashback = balance * 0.10;
         }
-        double finalBalance = balance + cashback;
+        double final = balance + cashback;
 
-        System.out.println("Cashback : ₹" + cashback);
-        System.out.println("Final Balance : ₹" + finalBalance);
+        System.out.println("Cashback : " + cashback);
+        System.out.println("Final Balance : " + final);
 
 
     }
