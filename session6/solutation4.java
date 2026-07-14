5b993d702539955386da900af6f228c9cb99c6d8
Rajpara Jatin <jatinrajpara2@gmail.com>
2026-07-14 11:28:23 +0530

    Update solutation4.java

--- session6/solutation4.java
+++ session6/solutation4.java
@@ -3,21 +3,21 @@ package session6;
 public class solutation4 {
 
     String movieTitle;
-    String seatNumber;
+    String seatNo;
 
-    solutation4(String movieTitle, String seatNumber) {
+    solutation4(String movieTitle, String seatNo) {
         this.movieTitle = movieTitle;
-        this.seatNumber = seatNumber;
+        this.seatNo = seatNo;
     }
 
     void printTicket() {
         System.out.println("Movie Title : " + movieTitle);
-        System.out.println("Seat Number : " + seatNumber);
+        System.out.println("Seat Number : " + seatNo);
     }
 
     public static void main(String[] args) {
 
-        solutation4 ticket = new solutation4("Avengers: Endgame", "B12");
+        solutation4 ticket = new solutation4("Avengers: DoomsDay", "A09");
 
         ticket.printTicket();
     }
