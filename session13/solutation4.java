abcd6f4075adf95be4e24497d9f02a6092b07903
Rajpara Jatin <jatinrajpara2@gmail.com>
2026-07-14 13:08:35 +0530

    Update solutation4.java

--- session13/solutation4.java
+++ session13/solutation4.java
@@ -4,20 +4,20 @@ public class solutation4 {
 
     public static void main(String[] args) {
 
-        int[] transactions = {500, -200, 700, -100, 1000};
+        int[] transactions = {700,-800,600,-200,900};
 
-        for (int amount : transactions) {
+       for (int i = 0; i < transactions.length; i++) {
+            
+            int amount = transactions[i];
 
             try {
-
-                if (amount < 0) {
+                  if (amount < 0) {
                     throw new Exception("Invalid Transaction Amount: " + amount);
                 }
 
-                System.out.println("Transaction Processed: " + amount);
+                  System.out.println("Transaction Processed: " + amount);
 
             } catch (Exception e) {
-
                 System.out.println(e.getMessage());
 
             } finally {
